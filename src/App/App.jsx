a91e7f3274d55994098bc7cf71a40d58632070e0
Liam R <liamroberts77@googlemail.com>
2022-01-28 16:44:27 +0000

    added game.css

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+
+import Game from './components/Game/Game';
+
+const App = () => (
+        <div className="App container">
+          <Game />
+        </div>
+);
+
+export default App;
